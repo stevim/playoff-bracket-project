da0988b27ed36f774fdbd5c6062ea37f72f883fc
stevim <i.m.steveim@gmail.com>
2023-04-22 14:18:22 -0400

    rename directory

---
+++ models/profile.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const profileSchema = new Schema({
+  name: String,
+  avatar: String,
+}, {
+  timestamps: true
+})
+
+const Profile = mongoose.model('Profile', profileSchema)
+
+
+export {
+  Profile
+}
