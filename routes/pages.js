5ae26f9f0b765e245f6a71f090469a5aa3ae734a
stevim <i.m.steveim@gmail.com>
2023-04-25 13:54:31 -0400

    protect routes

--- routes/pages.js
+++ routes/pages.js
@@ -1,32 +1,33 @@
 import { Router } from 'express'
 import * as pagesCtrl from "../controllers/pages.js"
+import { isLoggedIn } from "../middleware/middleware.js"
 
 const router = Router()
 
 // GET localhost:3000/pages
 router.get('/', pagesCtrl.index)
 // GET localhost:3000/pages/new
-router.get('/new', pagesCtrl.new)
+router.get('/new', isLoggedIn, pagesCtrl.new)
 // GET localhost:3000/pages/:pageId
 router.get('/:pageId', pagesCtrl.show)
 // GET localhost:3000/pages/:pageId/edit
-router.get('/:pageId/edit', pagesCtrl.edit)
+router.get('/:pageId/edit', isLoggedIn, pagesCtrl.edit)
 // POST localhost:3000/pages
-router.post('/', pagesCtrl.create)
+router.post('/', isLoggedIn, pagesCtrl.create)
 // POST localhost:3000/pages/:pageId/comments
 router.post('/:pageId/comments', pagesCtrl.createComment)
 // POST localhost:3000/pages/:pageId/teams
-router.post('/:pageId/teams', pagesCtrl.addToFavTeams)
+router.post('/:pageId/teams', isLoggedIn, pagesCtrl.addToFavTeams)
 // POST localhost:3000/pages/:pageId/athletes
-router.post('/:pageId/athletes', pagesCtrl.addToFavAthletes)
+router.post('/:pageId/athletes', isLoggedIn, pagesCtrl.addToFavAthletes)
 // DELETE localhost:3000/pages/:pageId
-router.delete('/:pageId', pagesCtrl.delete)
+router.delete('/:pageId', isLoggedIn, pagesCtrl.delete)
 // DELETE localhost:3000/pages/:pageId/comments/:commentId
-router.delete('/:pageId/comments/:commentId', pagesCtrl.deleteComment)
+router.delete('/:pageId/comments/:commentId', isLoggedIn, pagesCtrl.deleteComment)
 // DELETE localhost:3000/pages/:pageId/teams/:teamId
-router.delete('/:pageId/teams/:teamId', pagesCtrl.deleteTeam)
+router.delete('/:pageId/teams/:teamId', isLoggedIn, pagesCtrl.deleteTeam)
 // DELETE localhost:3000/pages/:pageId/athletes/:athleteId
-router.delete('/:pageId/athletes/:athleteId', pagesCtrl.deleteAthlete)
+router.delete('/:pageId/athletes/:athleteId', isLoggedIn, pagesCtrl.deleteAthlete)
 
 // PUT localhost:3000/pages/:pageId
 router.put('/:pageId', pagesCtrl.update)
