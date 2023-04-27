c0765f2382fef4816ee097ee87a2efb7ffbfd08b
stevim <i.m.steveim@gmail.com>
2023-04-27 16:02:40 -0400

    complete teams index and show views

--- controllers/teams.js
+++ controllers/teams.js
@@ -14,6 +14,19 @@ function newTeam(req,res) {
   })
 }
 
+function index(req, res) {
+  Team.find({})
+  .then(teams => {
+    res.render('teams/index', {
+      teams: teams,
+      title: 'Teams'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/teams')
+  })
+}
 
 function create(req,res) {
   Team.create(req.body)
@@ -26,7 +39,85 @@ function create(req,res) {
   })
 }
 
+function show(req, res) {
+  Team.findById(req.params.teamId)
+  .populate([
+    {
+      path: 'comments',
+      populate: {
+        path: 'creator',
+        model: 'Profile'
+      }
+    }
+  ])
+  .then(team => {
+    res.render('teams/show', {
+      team,
+      title: team.name,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/teams')
+  })
+}
+
+function deleteTeam(req, res) {
+  Team.findByIdAndDelete(req.params.teamId)
+  .then(team => {
+    res.redirect('/teams/index')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/teams')
+  })
+}
+
+function createComment(req, res) {
+  req.body.creator=req.user.profile._id
+  Team.findById(req.params.teamId)
+  .then(team => {
+    team.comments.push(req.body)
+    team.save()
+    .then(()=> {
+      res.redirect(`/teams/${team._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function deleteComment(req, res) {
+  Team.findById(req.params.teamId)
+  .then(team => {
+    team.comments.remove(req.params.commentId)
+    team.save()
+    .then(() => {
+      res.redirect(`/teams/${team._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/teams')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/teams')
+  })
+}
+
 export {
   create,
+  index,
+  show,
   newTeam as new,
+  deleteTeam as delete,
+  createComment,
+  deleteComment,
 }
--- routes/teams.js
+++ routes/teams.js
@@ -1,10 +1,21 @@
 import { Router } from "express"
 import * as teamsCtrl from '../controllers/teams.js'
+import { isLoggedIn } from "../middleware/middleware.js"
 
 const router = Router()
 
 router.get('/new', teamsCtrl.new)
 
+router.get('/index', teamsCtrl.index)
+
+router.get('/:teamId', teamsCtrl.show)
+
 router.post('/', teamsCtrl.create)
+
+router.post('/:teamId/comments', teamsCtrl.createComment)
+
+router.delete('/:teamId', isLoggedIn, teamsCtrl.delete)
+
+router.delete('/:teamId/comments/:commentId', isLoggedIn, teamsCtrl.deleteComment)
 
 export { router }
